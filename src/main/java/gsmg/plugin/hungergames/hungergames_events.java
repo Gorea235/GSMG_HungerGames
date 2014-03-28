783e29c001be86707622512287b0f34610e153ed
Gorea235 <lkdvs3@gmail.com>
2014-03-28 20:11:46 +0000

    Added some basic functions

---
+++ src/main/java/gsmg/plugin/hungergames/hungergames_events.java
@@ -0,0 +1,8 @@
+package gsmg.plugin.hungergames;
+
+import org.bukkit.event.Listener;
+import org.bukkit.event.EventHandler;
+
+public class hungergames_events implements Listener {
+
+}
--- src/main/java/gsmg/plugin/hungergames/hungergames_main.java
+++ src/main/java/gsmg/plugin/hungergames/hungergames_main.java
@@ -1,7 +1,16 @@
 package gsmg.plugin.hungergames;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class hungergames_main extends JavaPlugin {
+	private hungergames_main plugin = hungergames_main.this;
+	
+	public void broadcastMessage(String msg) {
+		Bukkit.broadcastMessage("[GSMG - Hunger Games] " + msg);
+	}
 
+	public static void Log(String toLog) {
+		Bukkit.getLogger().info(toLog);
+	}
 }
